fc1a2e3d8b5fc13ed31aef2d1edf3a40430f4e60
Khushi Jain <khushij1011@gmail.com>
2024-07-06 14:07:06 +0530

    transaction model

--- models/transactionModel.js
+++ models/transactionModel.js
@@ -2,5 +2,28 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const transactionSchema = mongoose.Schema({
-    userId: Schema.Types.ObjectId,
+    userId: {
+        type: Schema.Types.ObjectId,
+        ref: 'userModel',
+    },
+    serviceId: {
+        type: Schema.Types.ObjectId,
+        ref: 'serviceModel',
+    },
+    status: {
+        type: String,
+        enum: ["Success", "Pending", "Failed"],
+    },
+    date: {
+        type: String,
+        required: true,
+    },
+    amount: {
+        type: String,
+        required: true,
+    },
+    // requestBtn: {
+    //     type: Boolean,
+    //     default: false,
+    // }
 })
